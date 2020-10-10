ba71a3df178c743b3987fc49b1f74dc32362d717
yapie <targaryen.akane@gmail.com>
2020-10-11 00:42:19 +0700

    spoken_lang and clean up

--- src/routes/repo.rs
+++ src/routes/repo.rs
@@ -1,28 +1,9 @@
 use gtrend::gtrend::Since;
 use gtrend::repo;
-use rocket_contrib::json::Json;
-// use serde::{Deserialize, Serialize};
 use rocket::http::RawStr;
+use rocket_contrib::json::Json;
 use serde_json::{json, Value};
 use std::error::Error;
-// #[get("/gempa/<data>")]
-// pub fn gempa_data(data: String) -> Result<Json<Value>, Box<dyn Error>> {
-// let url = match data.as_str() {
-// "autogempa" => Autogempa,
-// "gempaterkini" => GempaTerkini,
-// "gempadirasakan" => GempaDirasakan,
-// "lasttsunami" => LastTsunami,
-// "en_autogempa" => EnAutogempa,
-// "en_gempaterkini" => EnGempaTerkini,
-// _ => Autogempa,
-// };
-// let data = gempa::get_data(url).and_then(gempa::to_json);
-
-// match data {
-// Ok(val) => Ok(Json(json!({ "data": val }))),
-// Err(e) => Err(e),
-// }
-// }
 
 #[get("/")]
 pub fn repo_index() -> Result<Json<Value>, Box<dyn Error>> {
@@ -68,10 +49,11 @@ pub fn repo_index() -> Result<Json<Value>, Box<dyn Error>> {
 // }
 // }
 
-#[get("/repositories?<language>&<since>")]
+#[get("/repositories?<language>&<since>&<spoken_lang>")]
 pub fn repo_repositories(
     language: Option<&RawStr>,
     since: Option<String>,
+    spoken_lang: Option<&RawStr>,
 ) -> Result<Json<Value>, Box<dyn Error>> {
     let s = since.map(|x| match x.as_str() {
         "daily" => Since::Daily,
@@ -79,21 +61,14 @@ pub fn repo_repositories(
         "monthly" => Since::Monthly,
         _ => Since::Daily,
     });
-
     let lang: Option<&str> = language.map(|x| x.as_str());
-    let data = repo::get_data(lang, s.unwrap_or(Since::Daily), None);
+    let s_lang: Option<&str> = spoken_lang.map(|x| x.as_str());
+
+    let data = repo::get_data(lang, s.unwrap_or(Since::Daily), s_lang);
 
     match data {
         Ok(val) => {
-            let x: Vec<_> = val
-                .clone()
-                .into_iter()
-                .map(|i| {
-                    // let serialized = serde_json::to_string(&i).unwrap();
-                    // println!("serialized = {}", serialized);
-                    json!(i)
-                })
-                .collect();
+            let x: Vec<_> = val.clone().into_iter().map(|i| json!(i)).collect();
 
             Ok(Json(Value::Array(x)))
         }
