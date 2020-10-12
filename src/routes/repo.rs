ef45520ea8e513b66f8036e8ef078e0a767536b3
yapie <targaryen.akane@gmail.com>
2020-10-13 00:24:47 +0700

    change response

--- src/routes/repo.rs
+++ src/routes/repo.rs
@@ -4,23 +4,47 @@ use rocket_contrib::json::Json;
 use serde_json::{json, Value};
 use std::error::Error;
 
+fn to_json(repos: Vec<repos::Repository>) -> Json<Value> {
+    let x: Vec<_> = repos
+        .into_iter()
+        .map(|x| {
+            json!({
+                "author": x.author,
+                "name": x.name,
+                "avatar": x.avatar,
+                "description": x.description,
+                "url": x.url,
+                "language": x.programming_language,
+                "languageColor": x.lang_color,
+                "stars": x.stars,
+                "forks": x.forks,
+                "currentPeriodStars": x.current_star,
+                "builtBy": x.build_by
+            })
+        })
+        .collect();
+
+    Json(Value::Array(x))
+}
 #[get("/")]
 pub fn repo_index() -> Result<Json<Value>, Box<dyn Error>> {
     let data = repos::get_data(None, Since::Daily, None);
 
     match data {
         Ok(val) => {
-            let x: Vec<_> = val
-                .clone()
-                .into_iter()
-                .map(|i| {
-                    // let serialized = serde_json::to_string(&i).unwrap();
-                    // println!("serialized = {}", serialized);
-                    json!(i)
-                })
-                .collect();
+            // let x: Vec<_> = val
+            // .clone()
+            // .into_iter()
+            // .map(|i| {
+            // // let serialized = serde_json::to_string(&i).unwrap();
+            // // println!("serialized = {}", serialized);
+            // json!(i)
+            // })
+            // .collect();
 
-            Ok(Json(Value::Array(x)))
+            let x = to_json(val);
+            // Ok(Json(Value::Array(x)))
+            Ok(x)
         }
         Err(e) => Err(e),
     }
@@ -67,9 +91,11 @@ pub fn repo_repositories(
 
     match data {
         Ok(val) => {
-            let x: Vec<_> = val.clone().into_iter().map(|i| json!(i)).collect();
+            // let x: Vec<_> = val.clone().into_iter().map(|i| json!(i)).collect();
 
-            Ok(Json(Value::Array(x)))
+            let x = to_json(val);
+            // Ok(Json(Value::Array(x)))
+            Ok(x)
         }
         Err(e) => Err(e),
     }
