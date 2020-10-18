7cdb8860a84c02fb036cffbf45ded0d762b267ef
yapie <targaryen.akane@gmail.com>
2020-10-18 22:28:22 +0700

    update gtrend

--- src/routes/languages.rs
+++ src/routes/languages.rs
@@ -1,15 +1,10 @@
+use gtrend::languages;
 use rocket_contrib::json::Json;
 use serde_json::Value;
 use std::error::Error;
-use std::fs::File;
-use std::io::Read;
 
 #[get("/languages")]
 pub fn languages() -> Result<Json<Value>, Box<dyn Error>> {
-    let mut file = File::open("languages.json")?;
-    let mut data = String::new();
-    file.read_to_string(&mut data)?;
-
-    let json_: Value = serde_json::from_str(&data).unwrap();
-    Ok(Json(json_))
+    let data: Value = languages::get_data_json();
+    Ok(Json(data))
 }
--- src/routes/spoken_languages.rs
+++ src/routes/spoken_languages.rs
@@ -1,15 +1,10 @@
+use gtrend::spoken_languages;
 use rocket_contrib::json::Json;
 use serde_json::Value;
 use std::error::Error;
-use std::fs::File;
-use std::io::Read;
 
 #[get("/spoken_languages")]
 pub fn spoken_languages() -> Result<Json<Value>, Box<dyn Error>> {
-    let mut file = File::open("spoken-languages.json")?;
-    let mut data = String::new();
-    file.read_to_string(&mut data)?;
-
-    let json_: Value = serde_json::from_str(&data).unwrap();
-    Ok(Json(json_))
+    let data_json: Value = spoken_languages::get_data_json();
+    Ok(Json(data_json))
 }
