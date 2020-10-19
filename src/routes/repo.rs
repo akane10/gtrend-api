f0bca74012be2c9cb05d2d0694c174a774ce5ee4
yapie <targaryen.akane@gmail.com>
2020-10-19 18:11:48 +0700

    purge cache if more than 1h and create folder .cache if it doesnt exist

--- src/routes/repo.rs
+++ src/routes/repo.rs
@@ -3,17 +3,43 @@ use gtrend::Since;
 use rocket_contrib::json::Json;
 use serde_json::{json, Value};
 use std::error::Error;
-use std::fs::File;
+use std::fs;
+use std::fs::{metadata, File};
 use std::io::Read;
+use std::time::Duration;
+use std::time::SystemTime;
 
 fn write_json(path: &str, data: &Value) -> Result<(), Box<dyn Error>> {
     // https://programming-idioms.org/idiom/92/save-object-into-json-file/2347/rust
+    let is_exist = fs::metadata(".cache").is_ok();
+
+    if !is_exist {
+        fs::create_dir(".cache")?;
+    }
+
     let val = &File::create(path)?;
     serde_json::to_writer(val, &data)?;
     Ok(())
 }
 
 fn read_json(path: &str) -> Result<Value, Box<dyn Error>> {
+    let meta = metadata(path)?;
+
+    if let Ok(time) = meta.created() {
+        let sys_time = SystemTime::now();
+        let difference = sys_time.duration_since(time).unwrap();
+
+        println!("{:?}", difference);
+        println!("{:?}", time);
+
+        if difference > Duration::new(3600, 0) {
+            fs::remove_file(path)?;
+        }
+    }
+    // else {
+    // println!("Not supported on this platform or filesystem");
+    // };
+
     let mut file = File::open(path)?;
     let mut data = String::new();
     file.read_to_string(&mut data)?;
